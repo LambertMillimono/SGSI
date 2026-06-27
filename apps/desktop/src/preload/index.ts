afe1ee1166b833841bfebf3b3a62af05b7da81dd
LambertMillimono <lambertmillimono8@gmail.com>
2026-06-27 11:39:40 +0000

    fix: expose ipc.on and ipc.removeListener in preload - fixes black screen

--- apps/desktop/src/preload/index.ts
+++ apps/desktop/src/preload/index.ts
@@ -4,5 +4,11 @@ contextBridge.exposeInMainWorld('electron', {
   ipc: {
     invoke: (channel: string, ...args: unknown[]) =>
       ipcRenderer.invoke(channel, ...args),
+    on: (channel: string, listener: (...args: unknown[]) => void) => {
+      ipcRenderer.on(channel, (_event, ...args) => listener(_event, ...args))
+    },
+    removeListener: (channel: string, listener: (...args: unknown[]) => void) => {
+      ipcRenderer.removeAllListeners(channel)
+    },
   },
 })
